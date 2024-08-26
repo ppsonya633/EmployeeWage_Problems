103ea087a4e98e92acaad763fd7f3c521d980d56
p sonya <psonya633@gmail.com>
2024-08-26 10:19:26 +0530

    [Pratik] add: refactor the code using class methods and variables

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -10,113 +10,119 @@
 
 import random
 
-
-WAGE_PER_HOUR=20
-DAILY_HOUR=8
-PART_TIME_HOUR=4
-MONTHLY_WORKING_DAYS=20
-
-
-def check_attendance():
-    """
-    Description:
-        this function is used for check attendance based on values 1 and 0
-    Return:
-        it will return a number randomely either 1 or 0
-    """
-
-    attendance=random.choice([0,0.5,1])
-    return attendance
+class Employee:
+    WAGE_PER_HOUR=20
+    DAILY_HOUR=8
+    PART_TIME_HOUR=4
+    MONTHLY_WORKING_DAYS=20
 
 
-def daily_wage():
-    """
-    Description:
-        This function gives an daily wage on employee
-    Return:
-        it will return an daily wage of employee
-    """
+    @classmethod
+    def check_attendance(cls):
+        """
+        Description:
+            this function is used for check attendance based on values 1 and 0
+        Parameter:
+            None
+        Return:
+            it will return a number randomely either 1 or 0
+        """
 
-    return WAGE_PER_HOUR*DAILY_HOUR
-
-    
-def partTime_wage():
-    """
-    Description:
-        This Program gives the part time wages of the employee
-    Parameter:
-        it will take employee attendance as a parameter
-    Return:
-        it will return an parttime wage of the employee
-    """
-    
-    return WAGE_PER_HOUR*PART_TIME_HOUR
+        attendance=random.choice([0,0.5,1])
+        return attendance
     
 
-def monthly_wage():
-    """
-    Description:
-        This Program will give a employee wages for a month
-    Parameter:
-        None
-    Return:
-        this function will return a monthely wages
-    """
+    @classmethod
+    def daily_wage(cls):
+        """
+        Description:
+            This function gives an daily wage on employee
+        Parameter:
+            None
+        Return:
+            it will return an daily wage of employee
+        """
 
-    total_wage=0
-    for i in range(MONTHLY_WORKING_DAYS):
-        attendence=check_attendance()
-        if attendence==1:
-            total_wage+=daily_wage()
-        elif attendence==0.5:
-            total_wage+=partTime_wage()
-        else:
-            total_wage+=0
+        return cls.WAGE_PER_HOUR*cls.DAILY_HOUR
 
-    return total_wage
+    
+    @classmethod
+    def partTime_wage(cls):
+        """
+        Description:
+            This Program gives the part time wages of the employee
+        Parameter:
+            None
+        Return:
+            it will return an parttime wage of the employee
+        """
 
+        return cls.PART_TIME_HOUR*cls.WAGE_PER_HOUR
+    
 
-def wages_ReachedForMonth():
-    """
-    Description:
-        This Program gives a wages until employee complete 100 working hours or 20 working days
-    Parameter:
-        None
-    Return:
-        gives a total wage of employee for one month when days or working hours got complete
-    """
+    @classmethod
+    def monthly_wage(cls):
+        """
+        Description:
+            This Program will give a employee wages for a month
+        Parameter:
+            None
+        Return:
+            this function will return a monthely wages
+        """
 
-    working_hour=0
-    working_days=0
-    total_wage=0
-
-    while working_hour<100 or working_days<20:
-
-        attendence=check_attendance()
-
-        match attendence:
-            case 1:
-                working_hour+=DAILY_HOUR
-                working_days+=1
-                total_wage+=daily_wage()
-
-            case 0.5:
-                working_hour+=PART_TIME_HOUR
-                working_days+=1
-                total_wage+=partTime_wage()
-
-            case 0:
-                working_hour+=0
-                working_days+=1
+        total_wage=0
+        for day in range(cls.MONTHLY_WORKING_DAYS):
+            attendence=cls.check_attendance()
+            if attendence==1:
+                total_wage+=cls.daily_wage()
+            elif attendence==0.5:
+                total_wage+=cls.partTime_wage()
+            else:
                 total_wage+=0
 
-    return total_wage
-
-
-def main():
-
-    print(f"Wages for employee when working hour or days reached for month: {wages_ReachedForMonth()}")
+        return total_wage
     
 
-if __name__=="__main__":
-    main()
+    @classmethod
+    def WagesFor_ComplitionOfHoursOrDaysForMonth(cls):
+        """
+        Description:
+            this function will gives a wages of employee when he completes the working hour or days in month
+        Parameter:
+            None
+        Return:
+            it will return wages when employees total monthly working hours or days get completed
+        """
+        
+        working_hour=0
+        working_days=0
+        total_wage=0
+
+        while working_hour<100 or working_days<20:
+
+            attendence=cls.check_attendance()
+
+            match attendence:
+                case 1:
+                    working_hour+=cls.DAILY_HOUR
+                    working_days+=1
+                    total_wage+=cls.daily_wage()
+
+                case 0.5:
+                    working_hour+=cls.PART_TIME_HOUR
+                    working_days+=1
+                    total_wage+=cls.partTime_wage()
+
+                case 0:
+                    working_hour+=0
+                    working_days+=1
+                    total_wage+=0
+
+        return total_wage
+
+
+print(f"wages when employee complete his total working hours or days for month :{Employee.WagesFor_ComplitionOfHoursOrDaysForMonth()}")
+       
+        
+
