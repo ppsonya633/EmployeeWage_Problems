04fef72aeadbc0862f6954939d97baf7b7069c12
p sonya <psonya633@gmail.com>
2024-08-27 21:52:54 +0530

    [Pratik] add: added the class method insted of class variable

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -14,106 +14,108 @@ import random
 
 class Employee:
 
-
     @classmethod
     def check_attendance(cls):
         """
         Description:
-            this function is used for check attendance based on values 1 and 0
+            This function is used to check attendance based on values 1, 0.5, and 0.
         Parameter:
             None
         Return:
-            it will return a number randomely either 1 or 0
+            It will return a number randomly: either 1 (full-time), 0.5 (part-time), or 0 (absent).
         """
-
-        attendance=random.choice([0,0.5,1])
+        attendance = random.choice([0, 0.5, 1])
         return attendance
-    
+
 
     @classmethod
-    def daily_wage(cls,wage_per_hour, daily_hour):
+    def daily_wage(cls, wage_per_hour, daily_hour):
         """
         Description:
-            This function gives an daily wage on employee
+            This function calculates the daily wage of an employee.
         Parameter:
-            wage_per_hour- wage_per hour of a companey
-            daily_hour-daily total working hour of a companey
+            wage_per_hour - wage per hour of a company
+            daily_hour - daily total working hours of a company
         Return:
-            it will return an daily wage of employee
+            It returns the daily wage of the employee.
         """
+        return wage_per_hour * daily_hour
 
-        return wage_per_hour*daily_hour
-
-    
-    @classmethod
-    def partTime_wage(cls,wage_per_hour,part_time_hour):
-        """
-        Description:
-            This Program gives the part time wages of the employee
-        Parameter:
-            wage_per_hour-per hour wage of a companey
-            part_time_hour-part time hour of a companey
-        Return:
-            it will return an parttime wage of the employee
-        """
-
-        return wage_per_hour*part_time_hour
-    
 
     @classmethod
-    def WagesFor_ComplitionOfHoursOrDaysForMonth(cls,wage_per_hour, daily_hour, part_time_hours, monthely_working_days,monthely_working_hour):
+    def partTime_wage(cls, wage_per_hour, part_time_hour):
         """
         Description:
-            this function will gives a wages of employee when he completes the working hour or days in month
+            This function calculates the part-time wages of the employee.
         Parameter:
-            wage_per_hour-per hour wage
-            daily_hour-daily working hours of a companey
-            part_time_hours-part time working hour
-            monthely_working_days-monthely working days for a companey
-            monthely_working_hour-monthely working hour of a companey
+            wage_per_hour - per hour wage of a company
+            part_time_hour - part-time hours of a company
         Return:
-            it will return wages when employees total monthly working hours or days get completed
+            It returns the part-time wage of the employee.
         """
-        
-        working_hour=0
-        working_days=0
-        total_wage=0
+        return wage_per_hour * part_time_hour
 
-        while working_hour<monthely_working_hour and working_days<monthely_working_days:
 
-            attendence=cls.check_attendance()
+    @classmethod
+    def WagesFor_ComplitionOfHoursOrDaysForMonth(cls, wage_per_hour, daily_hour, part_time_hours, monthly_working_days, monthly_working_hour):
+        """
+        Description:
+            This function calculates the wages of an employee when they complete the working hours or days in a month.
+        Parameter:
+            wage_per_hour - per hour wage
+            daily_hour - daily working hours of a company
+            part_time_hours - part-time working hour
+            monthly_working_days - monthly working days for a company
+            monthly_working_hour - monthly working hours of a company
+        Return:
+            It returns wages when an employee's total monthly working hours or days are completed.
+        """
+        working_hour = 0
+        working_days = 0
+        total_wage = 0
 
-            match attendence:
+        while working_hour < monthly_working_hour and working_days < monthly_working_days:
+
+            attendance = cls.check_attendance()
+
+            match attendance:
                 case 1:
-                    working_hour+=daily_hour
-                    total_wage+=cls.daily_wage(wage_per_hour, daily_hour)
-                    
+                    working_hour += daily_hour
+                    total_wage += cls.daily_wage(wage_per_hour, daily_hour)
+
                 case 0.5:
-                    working_hour+=part_time_hours
-                    total_wage+=cls.partTime_wage(wage_per_hour,part_time_hours)
-                    
+                    working_hour += part_time_hours
+                    total_wage += cls.partTime_wage(wage_per_hour, part_time_hours)
+
                 case 0:
-                    working_hour+=0
-                    total_wage+=0
+                    working_hour += 0
+                    total_wage += 0
 
-            working_days+=1
+            working_days += 1
 
-        return total_wage, working_days,working_hour
+        return total_wage, working_days, working_hour
+
+
+def main():
+
+    wage_per_hour = int(input("Enter the wage per hour for Company : "))
+    daily_hour = int(input("Enter the daily working hours for Company : "))
+    part_time_hours = int(input("Enter the part-time working hours for Company : "))
+    monthly_working_days = int(input("Enter the maximum working days for Company  in a month: "))
+    monthly_working_hour = int(input("Enter the maximum working hours for Company  in a month: "))
+
+    total_wage, working_days, working_hour = Employee.WagesFor_ComplitionOfHoursOrDaysForMonth(
+        wage_per_hour, daily_hour, part_time_hours, monthly_working_days, monthly_working_hour
+    )
+
+    print(f"Company  - Employees Total Wage: {total_wage}")
+    print(f"Company  - Employees Total Working Days: {working_days}")
+    print(f"Company  - Employees Total Working Hours: {working_hour}")
 
     
-def main():
-    total_wage, working_days,working_hour=Employee.WagesFor_ComplitionOfHoursOrDaysForMonth(10,8,4,24,100)
-
-    print(f"Employees Total Wage :{total_wage}")
-    print(f"Employees Total Working Days :{working_days}")
-    print(f"Employees Total Working Hours :{working_hour}")
-
-    total_wage,working_days,working_hour=Employee.WagesFor_ComplitionOfHoursOrDaysForMonth(20,10,5,28,150)
-    print(f"Employees Total Wage :{total_wage}")
-    print(f"Employees Total Working Days :{working_days}")
-    print(f"Employees Total Working Hours :{working_hour}")
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
+
+       
